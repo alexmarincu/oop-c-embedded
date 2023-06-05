5e77e663e44fd64917d60408f92028ccf5eec95d
Alex Marincu <alex.marincu@gmail.com>
2023-06-05 10:51:50 +0300

    Updated test_Timer

--- test/test_Timer.c
+++ test/test_Timer.c
@@ -2,7 +2,6 @@
 #include "unity.h"
 
 TEST_FILE("Timer.c")
-TEST_FILE("TimerClass.c")
 
 #include "mock_TimeSource.h"
 
@@ -11,6 +10,6 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-void test_App_example(void) {
+void test_Timer_example(void) {
     TEST_ASSERT_TRUE(1);
 }
