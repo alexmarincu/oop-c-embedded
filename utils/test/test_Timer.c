9004a503fcb04fa8235c3e873b4122fb55c91588
Alexandru Marincu <alexandru.marincu@continental-corporation.com>
2023-06-21 19:53:51 +0300

    Added test for Timer stop after expiry

--- utils/test/test_Timer.c
+++ utils/test/test_Timer.c
@@ -38,10 +38,21 @@ void test_Timer_should_be_running_and_expired_after_start_if_duration_is_elapsed
     TEST_ASSERT_TRUE(Timer_isRunning(t));
 }
 
-void test_Timer_should_not_be_running_or_expired_if_stopped(void) {
+void test_Timer_should_not_be_running_or_expired_if_stopped_before_expiration(void) {
     Timer * t = Timer_init(&(Timer){});
     TimeSource_getCurrentOpTimeIn1ms_ExpectAndReturn(100);
     Timer_start(t, 1000);
+    Timer_stop(t);
+    TEST_ASSERT_FALSE(Timer_isRunning(t));
+    TEST_ASSERT_FALSE(Timer_isExpired(t));
+}
+
+void test_Timer_should_not_be_running_or_expired_if_stopped_after_expiration(void) {
+    Timer * t = Timer_init(&(Timer){});
+    TimeSource_getCurrentOpTimeIn1ms_ExpectAndReturn(100);
+    Timer_start(t, 1000);
+    TimeSource_getCurrentOpTimeIn1ms_ExpectAndReturn(1100);
+    TEST_ASSERT_TRUE(Timer_isExpired(t));
     Timer_stop(t);
     TEST_ASSERT_FALSE(Timer_isRunning(t));
     TEST_ASSERT_FALSE(Timer_isExpired(t));
